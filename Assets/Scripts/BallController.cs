1a922dee3f1c2b63facf61d80e491c89b5037211
Shiba-dev <shiba.thehutt@gmail.com>
2021-07-27 15:39:53 -0300

    Controle da bola
    Controle básico para movimentação da bola

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField]
+    private float vel = 0.2f;
+    [SerializeField]
+    private Rigidbody rb;
+
+    void Awake()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+
+        rb.velocity = new Vector3(vel, 0, 0);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeMovement();
+        }
+    }
+
+    void ChangeMovement()
+    {
+        if(rb.velocity.x != 0)
+        {
+            rb.velocity = new Vector3(0, 0, vel);
+        }
+        else
+        {
+            rb.velocity = new Vector3(vel, 0, 0);
+        }
+    }
+}
