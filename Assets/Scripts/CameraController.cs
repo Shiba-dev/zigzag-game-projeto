2b350df95c4b94174fa48fc8b3a2b553508d5742
Shiba-dev <shiba.thehutt@gmail.com>
2021-07-27 16:28:27 -0300

    Câmera que segue
    Agora a câmera segue a bola com um toque suave na movimentação

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform _targetTransform;
+    [SerializeField]
+    private Vector3 _dif;
+    [SerializeField]
+    private float _smooth;
+    [SerializeField]
+    private Vector3 _camPos, _targetPos;
+
+    void Start()
+    {
+        if (_targetTransform == null)
+        {
+            _targetTransform = GameObject.Find("Ball").GetComponent<Transform>();
+        }
+
+        _dif = _targetTransform.position - transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        SmoothFunction();
+    }
+
+    void SmoothFunction()
+    {
+        _targetPos = _targetTransform.position - _dif;
+        _camPos = Vector3.Lerp(transform.position, _targetPos, _smooth * Time.deltaTime);
+        transform.position = _camPos;
+    }
+}
